c164a99f05d2ae7bb0e51429cf3edb2007dbe914
Frederik Aust <frederik.aust@uni-koeln.de>
2019-04-29 19:46:03 +0200

    Adds localization of replac_ampersands.

--- inst/replace_ampersands.lua
+++ inst/replace_ampersands.lua
@@ -1,10 +1,29 @@
-function Cite (el)
+local lang_tag = nil
+
+local and_dictionary = {
+  de = "und",
+  en = "and",
+  nl = "en",
+  es = "y"
+}
+
+function get_lang_tag (meta)
+  if meta.lang then
+    lang_tag = meta.lang[1].c:gsub("-%a+", "")
+  else
+    lang_tag = "en"
+  end
+end
+
+function replace_ampersands (el)
   if el.c[1][1].mode == "AuthorInText" then
     for key,value in pairs(el.c[2]) do
       for key2,value2 in pairs(value) do
-        if value2 == "&" then el.c[2][key][key2] = "and" end
+        if value2 == "&" then el.c[2][key][key2] = and_dictionary[lang_tag] end
       end
     end
   end
   return el
 end
+
+return {{Meta = get_lang_tag}, {Cite = replace_ampersands}}
