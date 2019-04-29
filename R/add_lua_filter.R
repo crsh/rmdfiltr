d44df6b5c22e53fca6dbba1926565b7521100f7f
Frederik Aust <frederik.aust@uni-koeln.de>
2019-04-29 15:40:14 +0200

    Minor improvements of R functions.

--- R/add_lua_filter.R
+++ R/add_lua_filter.R
@@ -1,52 +1,19 @@
-#' Verify minimum pandoc version
+#' Add Lua filter to pandoc arguments
 #'
-#' Verifies that pandoc version 2.0 or later is available.
-#'
-#' @param report Character. In case pandoc version is < 2.0 errors if
-#'   \code{error}, warns if \code{warn}, or remains silent otherwise.
-#'
-#' @return Logical. \code{TRUE} if pandoc version is 2.0 or later, \code{FALSE}
-#'   otherwise.
-#' @export
-#'
-#' @examples
-#'
-#' verify_pandoc_version(report = "silent")
-
-verify_pandoc_version <- function(report = "error") {
-  assertthat::assert_that(length(report) == 1)
-  assertthat::assert_that(is.character(report))
-
-  lua_available <- utils::compareVersion("2.0", as.character(rmarkdown::pandoc_version())) <= 0
-
-  report_fun <- switch(
-    report
-    , "error" = stop
-    , "warn" = function(x) { warning(x); return(FALSE) }
-    , function(x) return(FALSE)
-  )
-
-  if(!lua_available) {
-    report_fun("Lua-filters require pandoc 2.0 or later (https://pandoc.org/).")
-  }
-
-  lua_available
-}
-
-
-#' Add Lua-filter to pandoc arguments
-#'
-#' Adds a Lua-filter supplied by \pkg{rmdfiltr} to the vector of pandoc command
+#' Adds a Lua filter supplied by \pkg{rmdfiltr} to the vector of pandoc command
 #' line arguments.
 #'
 #' @param args Character. (Vector of) pandoc command line arguments.
-#' @param filter_name Character. Name of the Lua-filter to add. See details.
+#' @param filter_name Character. Name of the Lua filter to add. See details.
 #'
-#' @details The following Lua-filters are available. Convenience functions named
+#' @details The following Lua filters are available. Convenience functions named
 #'   after the filter are available (e.g. \code{add_*_filter()}).
 #'
 #' \describe{
-#'   \item{\code{}}{}
+#'   \item{\code{wordcount}}{The body of the text and reference sections are
+#'   counted separately. The word count for the text body does not contain,
+#'   tables or images (or their captions). The filter reports the word count in
+#'   the console or the R Markdown tab in RStudio.}
 #' }
 #'
 #' @return Character vector of pandoc command line arguments.
@@ -58,23 +25,20 @@ verify_pandoc_version <- function(report = "error") {
 #'
 #' add_lua_filter(NULL, "wordcount")
 
-add_lua_filter <- function(args, filter_name) {
+add_lua_filter <- function(args = NULL, filter_name) {
   if(!is.null(args)) assertthat::assert_that(is.character(args))
   assertthat::assert_that(length(filter_name) == 1)
   assertthat::assert_that(is.character(filter_name))
 
-  verify_pandoc_version()
-
-  filter_path <- system.file(filter_name, package = "rmdfiltr")
-  args <- c(args, "--lua-filter", filter_path)
+  if(verify_pandoc_version()) {
+    filter_path <- system.file(paste0(filter_name, ".lua"), package = "rmdfiltr")
+    args <- c(args, "--lua-filter", filter_path)
+  }
 
   args
 }
 
-#' Add Lua-filter to count words
-#'
-#' Adds a Lua-filter to count words to the vector of pandoc command line
-#' arguments.
+#' Add  to count words
 #'
 #' @inheritParams add_lua_filter
 #'
@@ -86,6 +50,6 @@ add_lua_filter <- function(args, filter_name) {
 #' @examples
 #' add_wordcount_filter(NULL)
 
-add_wordcount_filter <- function(args) {
+add_wordcount_filter <- function(args = NULL) {
   add_lua_filter(args, "wordcount")
 }
